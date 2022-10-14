3f90a95004756901cf738a67ebc6441bb60a8a52
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-10-14 23:14:38 +0300

    Task58

---
+++ Задача 58/Program.cs
@@ -0,0 +1,84 @@
+﻿/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int rows = GetNumberFromConsole("Введите количество строк");
+int columns = GetNumberFromConsole("Введите количество столбцов");
+int[,] array = new int[rows, columns];
+int[,] SecondArray = new int[rows, columns];
+int[,] result = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+FillArray(SecondArray);
+PrintArray(SecondArray);
+Console.WriteLine();
+
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < SecondArray.GetLength(1); j++)
+    {
+        result[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            result[i, j] += array[i, k] * SecondArray[k, j];
+        }
+    }
+  }
+
+PrintArray(result);
+
+
+//МЕТОДЫ
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue()
+{
+    int array = new Random().Next(1,10);
+    return array;
+}
+
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = GetRandomValue();
+        }
+    }
+}
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+
+
+
